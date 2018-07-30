dc1a140a2adeaade34bbd7b688076cef8f0be93e
Brendon Kuleck <brendon@fastmodeltechnologies.com>
2018-07-29 20:16:42 -0500

    Binding this

--- src/containers/SearchBar.js
+++ src/containers/SearchBar.js
@@ -3,10 +3,31 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
 export default class SearchBar extends Component {
+  constructor(props){
+    super(props);
+    
+    this.state = { term: '' };
+    
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+  
+ onInputChange(event){
+  console.log(event.target.value);
+  this.setState({ term: event.target.value });
+ }
+  
   render(){
     return(
       <form className="input-group">
-        <input />
+        <input
+          placeholder="Get a 5 Day Forecast in Your Favorite City"
+          className="form-control"
+          value={this.state.term}
+          onChange={this.onInputChange}
+        />
+        <span className="input-group-btn">
+          <button type="submit" className="btn btn-secondary">Submit</button>
+        </span>
       </form>
     );
   }
