0d9d695aac60996f124c06f32ce446c0b82f7d55
Brendon Kuleck <brendon@fastmodeltechnologies.com>
2018-07-29 22:11:32 -0500

    Openweather API call functioning

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,18 +1,16 @@
 import axios from 'axios';
 
 const API_KEY = 'aa6bc476907c4ec2b7a39042ce202662';
-const ROOT_URL =`https://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL =`https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
-export function fetchWeather(id){
-  const url = `${ROOT_URL}&id=${id}`;
-  // Will return a promise
+export function fetchWeather(term){
+  const url = `${ROOT_URL}&q=${term}`;
   const request = axios.get(url);
   
   return {
-    type: FETCH_WEATHER,
-    payload: request
+    type: FETCH_WEATHER
   };
 }
 
