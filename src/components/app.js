4eaa9fe026ea524f52c3ab453b2c4bf1db9c281d
Brendon Kuleck <brendon@fastmodeltechnologies.com>
2018-07-29 19:59:20 -0500

    SearchBar component

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import SearchBar from '../containers/SearchBar';
 
 export default class App extends Component {
   render() {
     return (
-      <div>Weatherly</div>
+      <div>
+        <SearchBar />
+      </div>
     );
   }
 }
