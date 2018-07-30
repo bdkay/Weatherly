6025dd803ade21c3300f1358e2a09049fa21fe97
Brendon Kuleck <brendon@fastmodeltechnologies.com>
2018-07-29 23:37:11 -0500

    Better comments, building a list container

--- src/containers/weather-list.js
+++ src/containers/weather-list.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 export default class WeatherList extends Component {
   render(){
@@ -7,6 +8,9 @@ export default class WeatherList extends Component {
         <thead>
           <tr>
             <th>City</th>
+            <th>Temperature</th>
+            <th>Pressure</th>
+            <th>Humidity</th>
           </tr>
         </thead>
         <tbody>
@@ -15,4 +19,11 @@ export default class WeatherList extends Component {
       </table>
     );
   }
+}
+
+// state.weather refactored
+function mapStateToProps({ weather }){
+  // Available because we assigned our WeatherReducer to the weather key in combined reducers
+  // { weather: weather } refactored
+  return { weather }
 }
