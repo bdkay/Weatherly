f8229113811454f9d2489b57423cbe7ebc3f7712
Brendon Kuleck <brendon@fastmodeltechnologies.com>
2018-07-29 22:51:37 -0500

    Refactor for state mutations in reducers

--- src/reducers/reducer-weather.js
+++ src/reducers/reducer-weather.js
@@ -1,4 +1,12 @@
-export default function(state = null, action){
-  console.log('Action received', action);
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [], action){
+  // action.payload.data
+  switch(action.type){
+    case(FETCH_WEATHER):
+      // Multiple cities (action.payload.data) === array
+      // return state.concat([action.payload.data]);
+      return [ action.payload.data, ...state ]; /// [city, city, city] NOT [city [city, city]]
+  }
   return state;
 }
