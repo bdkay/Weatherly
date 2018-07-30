d4485859a68f63667b8d7dda9831daffe0534cdd
Brendon Kuleck <brendon@fastmodeltechnologies.com>
2018-07-30 10:19:46 -0500

    Better comments:

--- src/reducers/reducer-weather.js
+++ src/reducers/reducer-weather.js
@@ -1,11 +1,13 @@
 import { FETCH_WEATHER } from '../actions/index';
 
 export default function(state = [], action){
+  
   // action.payload.data
   switch(action.type){
     case(FETCH_WEATHER):
       // Multiple cities (action.payload.data) === array
       // return state.concat([action.payload.data]);
+      
       return [ action.payload.data, ...state ]; /// [city, city, city] NOT [city [city, city]]
   }
   return state;
