ef3c448de98c2bcaf0e6b0676741c29e5264e3a9
Brendon Kuleck <brendon@fastmodeltechnologies.com>
2018-07-29 21:43:39 -0500

    Axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,14 @@
 const API_KEY = 'aa6bc476907c4ec2b7a39042ce202662';
+const ROOT_URL =`https://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(id){
+  const url = `${ROOT_URL}&id=${id}`;
+  
+  return {
+    type: FETCH_WEATHER
+  };
+}
+
+//4887398
